555e3738d09ad4876997ccd19c4afc1615be6085
Abir <majumdershankhadip@gmail.com>
2024-06-22 08:48:45 +0530

    initial commit

---
+++ src/app.py
@@ -0,0 +1,20 @@
+class Dummy(object):
+    def __init__(self,name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name  
+
+    @name.setter
+    def name(self,value):
+        if isinstance(value,str):
+            self.__name = value
+        else:
+            raise Exception ValueError("name can only be a string")
+
+if __name__ == "__main__":
+    obj = Dummy(name="Abir")
+    print(obj.name)
+    obj.name = "Sankhadip"
+    print(obj.name)
