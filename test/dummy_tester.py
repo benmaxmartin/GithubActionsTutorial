8b442ea0badf95b23c23e0b5c139ea138a76369e
Abir <majumdershankhadip@gmail.com>
2024-06-22 09:03:55 +0530

    added tests

---
+++ test/dummy_tester.py
@@ -0,0 +1,10 @@
+from src.app import Dummy
+import unittest
+
+class DummyTester(unittest.TestCase):
+    def test_name (self):
+        obj = Dummy(name = "Abir")
+        self.assertEqual(obj.name,"Abir")
+
+if __name__ == '__main__':
+    unittest.main()
